2f58b7f5df6ae4e36c3b926dea391ccb12ae8eb9
connor ingold <connoringold@gmail.com>
2018-07-13 01:30:58 +0100

    can now click on an image to enlarge it

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,10 +1,11 @@
-function zoomIn(event) {
+
+function zoomIn zoomIn((eventevent))  {{
   var element = document.getElementById("overlay");
   element.style.display = "inline-block";
   var img = document.getElementById("imgZoom");
   var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
   var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
-  element.style.backgroundPosition=(-posX*2)+"px "+(-posY*4)+"px";
+  element.style.backgroundPosition = (-posX * 4) + "px " + (-posY * 4) + "px";
 
 }
 
